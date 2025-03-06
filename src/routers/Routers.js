0c1a5044cc51a4049e42886339f727b404606ff9
Vamshi Krishna <t.k.vamshikrishna34@gmail.com>
2025-03-06 15:30:35 +0530

    Car Rental System

---
+++ src/routers/Routers.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import CarListing from "../pages/CarListing";
+import CarDetails from "../pages/CarDetails";
+import Blog from "../pages/Blog";
+import BlogDetails from "../pages/BlogDetails";
+import NotFound from "../pages/NotFound";
+import Contact from "../pages/Contact";
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Routers = () => {
+  useEffect(()=>{
+    Aos.init({duration:1000})
+  })
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/cars" element={<CarListing />} />
+      <Route path="/cars/:slug" element={<CarDetails />} />
+      <Route path="/blogs" element={<Blog />} />
+      <Route path="/blogs/:slug" element={<BlogDetails />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default Routers;
